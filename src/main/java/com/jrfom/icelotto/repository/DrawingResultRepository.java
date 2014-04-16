9ad5c0ad03cd4effb1908588f81795d16c98314c
James Sumners <james.sumners@gmail.com>
2014-04-16 11:59:48 -0400

    Removes useless "DrawingResult" objects

--- src/main/java/com/jrfom/icelotto/repository/DrawingResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jrfom.icelotto.repository;
-
-import com.jrfom.icelotto.model.DrawingResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DrawingResultRepository extends JpaRepository<DrawingResult, Long> {
-}
